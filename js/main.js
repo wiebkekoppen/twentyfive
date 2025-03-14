d5f07c4bbcfeec0dc9afe56ddc56b75fc6028e85
wiebkekoppen <wiebke.koppen@elev.ga.ntig.se>
2025-03-14 14:53:55 +0100

    Menubuttonfail
    Försökt sätts dit menubutton, har inte riktigt fungerat

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let layer = documeny.querySelector(".fade-layer")
+layer.addEventListener("click",showMenu)
+
+let button = document.querySelector(".menu-button")
+button.addEventListener("click",showMenu)
+
+function showMenu(){
+    let menu = document.querySelector("nav.menu")
+    menu.classList.toggle("show")
+
+    let layer = document.querySelector(".fade-layer")
+    layer.classList.toggle("visible")
+
+}

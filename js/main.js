706bfb6a4d6c92cb0332e4531ee8b1a4c603374b
wiebkekoppen <wiebke.koppen@elev.ga.ntig.se>
2025-03-28 14:28:01 +0100

    Text på bilder

--- js/main.js
+++ js/main.js
@@ -5,11 +5,12 @@ let button = document.querySelector(".menu-button")
 button.addEventListener("click",showMenu)
 
 function showMenu(){
-    let menu = document.querySelector("nav.menu")
+    let menu = document.querySelector(".menu")
     menu.classList.toggle("show")
 
     let layer = document.querySelector(".fade-layer")
     layer.classList.toggle("visible")
+   
 
 }
 
